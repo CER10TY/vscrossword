6e6b0b93933889e934f93a88f7b684e8496768c9
Sören Johanson <so.johanson@outlook.com>
2019-08-05 13:39:45 +0200

    initial empty

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,31 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+'use strict';
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscrossword" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('crossword.show', () => {
+			const panel = vscode.window.createWebviewPanel(
+				'crossword',
+				'Crossword',
+				vscode.ViewColumn.One
+			);
+			panel.webview.html = getWebviewContent();
+		})
+	);
 }
 
-// this method is called when your extension is deactivated
+function getWebviewContent() {
+	return `<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Cat Coding</title>
+</head>
+<body>
+	
+</body>
+</html>`;
+}
+
 export function deactivate() {}
